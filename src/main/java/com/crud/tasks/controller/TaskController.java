e9d1a0a01f2c3ec6b1921630f1ad46ce609f6cf2
szymonpp2016 <szymon.pp.2016@interia.pl>
2018-03-19 11:50:19 +0100

    module_14_4_REST_API1

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,35 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/task")
+public class TaskController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
+    public List<TaskDto> getTask(){
+        return new ArrayList<>();
+    }
+    @RequestMapping(method = RequestMethod.GET, value = "taskId")
+    public TaskDto getTask(String taskId) {
+        return new TaskDto((long) 1,"tes_title","test_content");
+    }
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
+    public void deleteTask(String taskId){
+    }
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
+    public TaskDto updateTask(TaskDto taskDto){
+        return new TaskDto((long) 1,"edit_title","edit_content");
+    }
+    @RequestMapping(method = RequestMethod.POST, value = "createTask")
+    public void createTask(TaskDto taskDto) {
+    }
+}
+
+
