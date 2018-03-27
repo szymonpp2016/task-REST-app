03b50d385925d76ca4975e9fc5b1bd3fc60c0e1d
szymonpp2016 <szymon.pp.2016@interia.pl>
2018-03-27 16:28:10 +0200

    module_16

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
