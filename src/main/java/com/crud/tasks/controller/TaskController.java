c57b8dd2908c053d5289e714413752da9fb3038a
szymonpp2016 <szymon.pp.2016@interia.pl>
2018-04-04 15:21:04 +0200

    module_17.3

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,6 @@
 package com.crud.tasks.controller;
 
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
