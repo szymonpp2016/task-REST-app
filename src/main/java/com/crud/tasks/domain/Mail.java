c985997057470392dcb13b0162f4020fb3517672
szymonpp2016 <szymon.pp.2016@interia.pl>
2018-04-23 21:42:35 +0200

    trello_modul_19_3_MAIL

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,19 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Mail {
+    private String mailTo;
+    private String toCc;
+    private String subject;
+    private String message;
+
+    public Mail(String mailTo, String subject, String message) {
+        this.mailTo = mailTo;
+        this.subject = subject;
+        this.message = message;
+    }
+}
