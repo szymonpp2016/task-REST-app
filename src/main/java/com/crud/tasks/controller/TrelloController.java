48b912eebe42ac436fb1c299e4aafeb967df9d8a
szymonpp2016 <szymon.pp.2016@interia.pl>
2018-04-25 10:24:24 +0200

    trello_modul_19_4_MAIL_CREATECARD_TRELLO_No_MAIL_SEND

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -7,9 +7,11 @@ import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.service.TrelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
 
+@ApiIgnore
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/v1/trello")
@@ -18,12 +20,12 @@ public class TrelloController {
     @Autowired
     private TrelloService trelloService;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/getTrelloBoards")
-    public List<TrelloBoardDto> getTrelloBoards() {
+    @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
+    public  List<TrelloBoardDto> getTrelloBoards() {
         return trelloService.fetchTrelloBoards();
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/createTrelloCard")
+    @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
     public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
         return trelloService.createTrelloCard(trelloCardDto);
     }
--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -34,9 +34,9 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-        if (mail.getToCc()!=null) {
-            mailMessage.setCc(mail.getToCc());
-        }
+    //    if (mail.getToCc()!=null) {
+     //       mailMessage.setCc(mail.getToCc());
+    //    }
         return mailMessage;
     }
 }
