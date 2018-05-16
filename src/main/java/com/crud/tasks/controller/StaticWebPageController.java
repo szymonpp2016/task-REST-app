5130f4e6237be6541587eff91f60251605dc493f
szymonpp2016 <szymon.pp.2016@interia.plgit config --global user.emainameszymon.pp.2016@interia.pl>
2018-05-16 16:34:08 +0200

    Mod_24_MAIL_SEND_THYMELEAF_INDEX_z1.v2

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -10,7 +10,7 @@ public class StaticWebPageController {
 
     @RequestMapping("/")
     public String index(Map<String, Object> model) {
-        model.put("variable", "My Thymeleaf variable");
+        model.put("variable", "My Thymeleaf variable:");
         model.put("one", 1);
         model.put("two", 2);
         return "index";}
